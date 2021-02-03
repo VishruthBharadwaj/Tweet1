8e439ebfe2f3db8d0c6ecc1dcca5394249cc6de0
VishruthBharadwaj <64675356+VishruthBharadwaj@users.noreply.github.com>
2021-02-03 16:00:53 +0530

    Add files via upload

---
+++ twitterkeys.py
@@ -0,0 +1,8 @@
+twitter = {
+			"consumer_key" : "Please enter key here",
+			"consumer_secret" : "Please enter key here",
+			"access_key" : "Please enter key here",
+			"access_secret" : "Please enter key here"
+				
+			}
+
